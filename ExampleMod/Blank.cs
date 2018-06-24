b6c88b5282166245d0c29def63e4b226000e2bff
MarshMello <33008390+MarshMello0@users.noreply.github.com>
2018-06-24 14:17:52 +0100

    Initial commit

---
+++ ExampleMod/Blank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using Harmony;
+using System.Reflection;
+
+public class Blank : Mod
+{
+    public Blank() : base("Name of Mod", "Blank", "1.0", "1.01B")
+    {
+        var harmony = HarmonyInstance.Create("com.raft.MODAUTHOR.MODNAME");
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+}
